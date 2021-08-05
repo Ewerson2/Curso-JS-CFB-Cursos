a991bb9c6faf266c1ce1912ccf26fbabdc87e9f3
Ewerson de Jesus <ewerson_dan@hotmail.com>
2021-08-04 21:01:05 -0300

    Window

---
+++ window/script.js
@@ -0,0 +1,9 @@
+ var minhaJanela
+ 
+ function abreJ(){
+ minhaJanela = window.open("../tmp", "_blank","width=300,height=300,top=300,left=300")
+}
+function fechaJ(){
+    minhaJanela.close()
+   }
+   
