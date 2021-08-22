39d6d59c747b7905f3e8c2aebf5d8e3c463a601c
Ewerson <ewerson_dan@hotmail.com>
2021-08-22 19:07:44 -0300

    Atualizando Curso

---
+++ objeto location p2/script.js
@@ -0,0 +1,13 @@
+
+function p2(){
+    location.replace("p2.html") //replace remove a url do histórico
+}
+
+function rec(){
+    location.reload()
+}
+
+function p2A(){
+    location.assign("p2.html") //assign não remove a url do histórico
+
+}
